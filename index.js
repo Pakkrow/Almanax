58a5fdc68c0f8222e1635ef77d7dbe5daa0f1170
Pakkrow <marvin.bellouard@epitech.eu>
2025-01-17 10:30:05 +0100

    ADD an Hello World to check deploy

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+    res.send("<h1> Hello World </h1>");
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
