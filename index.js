0475ed21effcd0bf2788f4ee16ccb22d4d084e92
Pakkrow <marvin.bellouard@epitech.eu>
2025-01-17 11:15:34 +0100

    [FIX] Deployment issues on Heroku

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-const express = require("express");
-
-// Creates an express object
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-// It listens to HTTP get request. 
-// Here it listens to the root i.e '/'
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
+    res.send("<h1> Hello World </h1>");
+  });
 
-  // Using send function we send
-  // response to the client
-  // Here we are sending html
-  res.send("<h1> Hello World </h1>");
-});
-
-// It configures the system to listen
-// to port 3000. Any number can be 
-// given instead of 3000, the only
-// condition is that no other server
-// should be running at that port
-app.listen(3000, () => {
-
-  // Print in the console when the
-  // servers starts to listen on 3000
-  console.log("Listening to port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
